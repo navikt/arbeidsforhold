f47895f8d3239f5b107d8153666bbecb67dd9c49
Morten Jansrud <morten@founder.no>
2019-05-13 15:53:33 +0200

    Første iterasjon visning av arbeidsforhold

--- src/Arbeidsforhold.tsx
+++ src/Arbeidsforhold.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Arbeidsforhold } from "./types/arbeidsforhold";
 import { AppProps } from "./index";
+import { Normaltekst, Element } from "nav-frontend-typografi";
+import Lenke from "nav-frontend-lenker";
 
 interface Props {
   arbeidsforhold: Arbeidsforhold[];
@@ -17,9 +19,34 @@ const Arbeidsforhold = (props: MergedProps) => {
         classNameContainer ? classNameContainer : ""
       }`}
     >
+      <div className="oversikt__rad">
+        <div className="oversikt__kolonne oversikt__tittel">
+          <Element>Arbeidsgiver</Element>
+        </div>
+        <div className="oversikt__kolonne">
+          <Element>Startdato</Element>
+        </div>
+        <div className="oversikt__kolonne">
+          <Element>Sluttdato</Element>
+        </div>
+        <div className="oversikt__kolonne" />
+      </div>
       {arbeidsforhold.map(foretak => (
-        <div key={foretak.navArbeidsforholdId}>
-          {foretak.arbeidsgiver.organisasjonsnummer}
+        <div className="oversikt__rad" key={foretak.navArbeidsforholdId}>
+          <div className="oversikt__kolonne oversikt__tittel">
+            <Normaltekst>
+              {foretak.arbeidsgiver.organisasjonsnummer}
+            </Normaltekst>
+          </div>
+          <div className="oversikt__kolonne">
+            <Normaltekst>{foretak.ansettelsesperiode.periode.fom}</Normaltekst>
+          </div>
+          <div className="oversikt__kolonne">
+            <Normaltekst>{foretak.ansettelsesperiode.periode.tom}</Normaltekst>
+          </div>
+          <div className="oversikt__kolonne">
+            <Lenke href="#test">Vis detaljer</Lenke>
+          </div>
         </div>
       ))}
     </div>

f7e13bdff8bbd8e49f069f3c03f55ba19328f4b3
Morten Jansrud <morten@founder.no>
2019-05-13 16:06:58 +0200

    Flytt noe design til eksempel-applikasjon

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,8 +5,8 @@ import Arbeidsforhold from "@navikt/arbeidsforhold";
 export default class App extends Component {
   render() {
     return (
-      <div className="app">
-        <div className="content">
+      <div className="example__app">
+        <div className="example__content">
           <Arbeidsforhold />
         </div>
       </div>

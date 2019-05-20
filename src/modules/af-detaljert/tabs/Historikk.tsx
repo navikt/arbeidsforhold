e7a9425431c55fcfd35fc7e1f760cfceb16e9e13
Morten Jansrud <morten@founder.no>
2019-05-20 14:59:09 +0200

    Omgjør historikk til en html tabell

--- src/modules/af-detaljert/tabs/Historikk.tsx
+++ src/modules/af-detaljert/tabs/Historikk.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AFArbeidsavtaler } from "../../../types/arbeidsforhold";
 import Moment from "react-moment";
+import { Element } from "nav-frontend-typografi";
 
 interface Props {
   arbeidsavtaler: AFArbeidsavtaler[];
@@ -9,19 +10,36 @@ interface Props {
 const Historikk = (props: Props) => {
   console.log(props);
   return (
-    <div className="af-detaljert__tabs-innhold af-liste__table">
+    <table className="af-detaljert__tabs-innhold af-liste__table">
+      <tr className="af-liste__rad">
+        <td className="af-liste__kolonne">
+          <Element>Yrke</Element>
+        </td>
+        <td className="af-liste__kolonne">
+          <Element>Stillingsprosent</Element>
+        </td>
+        <td className="af-liste__kolonne">
+          <Element>Periode</Element>
+        </td>
+        <td className="af-liste__kolonne">
+          <Element>Timer/uke</Element>
+        </td>
+        <td className="af-liste__kolonne">
+          <Element>Lønnsendring</Element>
+        </td>
+      </tr>
       {props.arbeidsavtaler.map(arbeidsavtale => (
-        <div
+        <tr
           className="af-liste__rad"
           key={`${arbeidsavtale.bruksperiode.fom}-${
             arbeidsavtale.bruksperiode.tom
           }`}
         >
-          <div className="af-liste__kolonne">{arbeidsavtale.yrke}</div>
-          <div className="af-liste__kolonne">
+          <td className="af-liste__kolonne">{arbeidsavtale.yrke}</td>
+          <td className="af-liste__kolonne">
             {arbeidsavtale.stillingsprosent}
-          </div>
-          <div className="af-liste__kolonne">
+          </td>
+          <td className="af-liste__kolonne">
             <Moment format="DD.MM.YYYY">
               {arbeidsavtale.bruksperiode.fom}
             </Moment>
@@ -29,18 +47,18 @@ const Historikk = (props: Props) => {
             <Moment format="DD.MM.YYYY">
               {arbeidsavtale.bruksperiode.tom}
             </Moment>
-          </div>
-          <div className="af-liste__kolonne">
+          </td>
+          <td className="af-liste__kolonne">
             {arbeidsavtale.antallTimerPrUke}
-          </div>
-          <div className="af-liste__kolonne">
+          </td>
+          <td className="af-liste__kolonne">
             <Moment format="DD.MM.YYYY">
               {arbeidsavtale.sistLoennsendring}
             </Moment>
-          </div>
-        </div>
+          </td>
+        </tr>
       ))}
-    </div>
+    </table>
   );
 };
 

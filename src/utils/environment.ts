d38b5791028fbf7f187e52f73696d07eab866112
Morten Jansrud <mjansrud@users.noreply.github.com>
2019-05-28 11:23:33 +0200

    Nytt endepunkt (#6)
    * Oppdater api-endepunkt

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -9,17 +9,17 @@ class Environment {
     switch (miljo) {
       default:
       case "LOCAL":
-        Environment.apiUrl = `http://localhost:8080/person/personopplysninger-api`;
+        Environment.apiUrl = `http://localhost:8080/person/arbeidsforhold-api`;
         Environment.loginUrl = `https://loginservice-q.nav.no/login`;
         Environment.logoutUrl = `https://loginservice-q.nav.no/slo`;
         break;
       case "DEV":
-        Environment.apiUrl = `https://www-q0.nav.no/person/personopplysninger-api`;
+        Environment.apiUrl = `https://www-q0.nav.no/person/arbeidsforhold-api`;
         Environment.loginUrl = `https://loginservice-q.nav.no/login`;
         Environment.logoutUrl = `https://loginservice-q.nav.no/slo`;
         break;
       case "PROD":
-        Environment.apiUrl = `https://www.nav.no/person/personopplysninger-api`;
+        Environment.apiUrl = `https://www.nav.no/person/arbeidsforhold-api`;
         Environment.loginUrl = `https://loginservice.nav.no/login`;
         Environment.logoutUrl = `https://loginservice.nav.no/slo`;
         break;

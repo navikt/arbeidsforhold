0657f1f94b24049d2f7eefb01e0841501740db22
Morten Jansrud <mjansrud@users.noreply.github.com>
2019-06-12 13:17:07 +0200

    Felles felter for arbeidsavtale (#29)
    * Riktig sortering i arbeidsforhold-listen
    
    * Felles felter

---
+++ src/components/arbeidsavtale/Felter.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import CheckAndPrintBox from "../check-and-print-box/CheckAndPrintBox";
+import CheckDateAndPrint from "../check-date-and-print/CheckDateAndPrint";
+import { AFArbeidsavtaler, AFUtvidet } from "../../types/arbeidsforhold";
+import { Normaltekst } from "nav-frontend-typografi";
+import sprak from "../../language/provider";
+
+interface Props {
+  locale: string;
+  data: AFArbeidsavtaler | AFUtvidet;
+}
+
+const ArbeidsavtaleFelter = (props: Props) => {
+  const { locale, data } = props;
+  return (
+    <>
+      <CheckAndPrintBox
+        title={sprak[locale].stillingsprosent}
+        data={data.stillingsprosent}
+      >
+        <Normaltekst>
+          <CheckDateAndPrint
+            data={data.sisteStillingsendring}
+            format={`(${sprak[locale].endretstillingsprosent} %s)`}
+          />
+        </Normaltekst>
+      </CheckAndPrintBox>
+      <CheckAndPrintBox
+        title={sprak[locale].arbeidstidsordning}
+        data={data.arbeidstidsordning}
+      />
+      <CheckAndPrintBox
+        title={sprak[locale].sistelonnsendring}
+        data={data.sisteLoennsendring}
+        date={true}
+      />
+      <CheckAndPrintBox
+        title={sprak[locale].timerifullstilling}
+        data={data.antallTimerPrUke}
+      />
+      <CheckAndPrintBox
+        title={sprak[locale].skipsregister}
+        data={data.skipsregister}
+      />
+      <CheckAndPrintBox title={sprak[locale].skipstype} data={data.skipstype} />
+      <CheckAndPrintBox
+        title={sprak[locale].fartsomraade}
+        data={data.fartsomraade}
+      />
+    </>
+  );
+};
+
+export default ArbeidsavtaleFelter;

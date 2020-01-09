10484d1992befc1aa7641eb394b863ed70a9077d
Morten Jansrud <morten.jansrud@nav.no>
2020-01-09 08:49:33 +0100

    Riktig login-url lokalt

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -3,15 +3,13 @@ import Miljo from "../types/miljo";
 class Environment {
   static apiUrl: string;
   static loginUrl: string;
-  static logoutUrl: string;
 
   static settEnv = (miljo: Miljo) => {
     switch (miljo) {
       default:
       case "LOCAL":
         Environment.apiUrl = `http://localhost:8096/person/arbeidsforhold-api`;
-        Environment.loginUrl = `https://loginservice-q.nav.no/login`;
-        Environment.logoutUrl = `https://loginservice-q.nav.no/slo`;
+        Environment.loginUrl = `http://localhost:5000`;
         break;
       case "Q0":
       case "Q1":
@@ -19,12 +17,10 @@ class Environment {
       case "Q6":
         Environment.apiUrl = `https://www-${miljo.toLowerCase()}.nav.no/person/arbeidsforhold-api`;
         Environment.loginUrl = `https://loginservice-q.nav.no/login`;
-        Environment.logoutUrl = `https://loginservice-q.nav.no/slo`;
         break;
       case "PROD":
         Environment.apiUrl = `https://www.nav.no/person/arbeidsforhold-api`;
         Environment.loginUrl = `https://loginservice.nav.no/login`;
-        Environment.logoutUrl = `https://loginservice.nav.no/slo`;
         break;
     }
   };

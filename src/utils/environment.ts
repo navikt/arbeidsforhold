52692c0d1a5b7dec59d4fff79de0c1b52ca2ffe1
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-03-16 15:15:42 +0100

    Oppdagert url til arbeidsforhold-api i dev

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -12,7 +12,7 @@ export class Environment {
         Environment.loginUrl = `http://localhost:5000`;
         break;
       case "DEV":
-        Environment.apiUrl = `https://person.dev.nav.no/person/arbeidsforhold-api`;
+        Environment.apiUrl = `https://person.ekstern.dev.nav.no/person/arbeidsforhold-api`;
         Environment.loginUrl = `https://loginservice.dev.nav.no`;
         break;
       case "PROD":

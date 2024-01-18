0406b3788aa10073fb62dd838d7d5ded65be257d
Terje Karlsen <terjeofnorway@gmail.com>
2024-01-18 19:59:44 +0100

    Fikser feil i React render til DOM

--- src/modules/af-liste/Liste.tsx
+++ src/modules/af-liste/Liste.tsx
@@ -1,63 +1,39 @@
-import React, { useState } from "react";
-import { AFListeData, AFListeProps } from "./index";
-import { sortPeriodeFraDesc, sortPeriodeTilDesc } from "../../utils/date";
-import { sprak } from "../../language/provider";
-import { useLocale } from "../common/useLocale";
-import { ListeInnslag } from "./innslag/ListeInnslag";
-import { VisFlereButton } from "./vis-flere-button/VisFlereButton";
-import { PrintButton } from "./print/PrintButton";
-import { BodyShort } from "@navikt/ds-react";
+import React, { useState } from 'react';
+import { AFListeData, AFListeProps } from './index';
+import { sortPeriodeFraDesc, sortPeriodeTilDesc } from '../../utils/date';
+import { sprak } from '../../language/provider';
+import { useLocale } from '../common/useLocale';
+import { ListeInnslag } from './innslag/ListeInnslag';
+import { VisFlereButton } from './vis-flere-button/VisFlereButton';
+import { PrintButton } from './print/PrintButton';
+import { BodyShort } from '@navikt/ds-react';
 
 export const Liste = (props: AFListeProps & AFListeData) => {
-  const { arbeidsforhold, onClick } = props;
-  const [visAlle, settVisAlle] = useState<boolean>(false);
-  const { locale } = useLocale();
+    const { arbeidsforhold, onClick } = props;
+    const [visAlle, settVisAlle] = useState<boolean>(false);
+    const { locale } = useLocale();
 
-  const sorterteArbeidsforhold = arbeidsforhold
-    .sort((a, b) =>
-      sortPeriodeFraDesc(
-        a.ansettelsesperiode.periode,
-        b.ansettelsesperiode.periode
-      )
-    )
-    .sort((a, b) =>
-      sortPeriodeTilDesc(
-        a.ansettelsesperiode.periode,
-        b.ansettelsesperiode.periode
-      )
+    const sorterteArbeidsforhold = arbeidsforhold
+        .sort((a, b) => sortPeriodeFraDesc(a.ansettelsesperiode.periode, b.ansettelsesperiode.periode))
+        .sort((a, b) => sortPeriodeTilDesc(a.ansettelsesperiode.periode, b.ansettelsesperiode.periode));
+
+    return (
+        <div className={`af-liste__container`}>
+            <div className="af-liste__table">
+                {sorterteArbeidsforhold.length > 0 &&
+                    sorterteArbeidsforhold
+                        .slice(0, visAlle ? arbeidsforhold.length : 5)
+                        .map((foretak, counter) => <ListeInnslag foretak={foretak} onClick={onClick} key={counter} />)}
+                {sorterteArbeidsforhold.length === 0 && (
+                    <div className="af-liste__flex-rad">
+                        <BodyShort>{sprak[locale].ingenarbeidsforhold}</BodyShort>
+                    </div>
+                )}
+            </div>
+            {arbeidsforhold.length > 5 && <VisFlereButton toggleVisAlle={() => settVisAlle(!visAlle)} visAlle={visAlle} />}
+            {props.printActivated && arbeidsforhold.length > 0 && (
+                <PrintButton arbeidsforhold={arbeidsforhold} printSSN={props.printSSN} printName={props.printName} />
+            )}
+        </div>
     );
-
-  return (
-    <div className={`af-liste__container`}>
-      <div className="af-liste__table">
-        {sorterteArbeidsforhold.length > 0 && (
-          sorterteArbeidsforhold
-            .slice(0, visAlle ? arbeidsforhold.length : 5)
-            .map((foretak, counter) => (
-              <ListeInnslag foretak={foretak} onClick={onClick} key={counter} />
-            ))
-        )}
-        {sorterteArbeidsforhold.length === 0 && (
-          <div className="af-liste__flex-rad">
-            <BodyShort>{sprak[locale].ingenarbeidsforhold}</BodyShort>
-          </div>
-        )}
-      </div>
-
-      {arbeidsforhold.length > 5 && (
-        <VisFlereButton
-          toggleVisAlle={() => settVisAlle(!visAlle)}
-          visAlle={visAlle}
-        />
-      )}
-
-      {props.printActivated && arbeidsforhold.length > 0 && (
-        <PrintButton
-          arbeidsforhold={arbeidsforhold}
-          printSSN={props.printSSN}
-          printName={props.printName}
-        />
-      )}
-    </div>
-  );
 };

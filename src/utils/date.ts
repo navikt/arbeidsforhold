96b6fe74dafd52a486953cc844144a4a0ffd575b
Bjørn Harbo Dahle <bdahle@hey.com>
2024-02-06 14:55:10 +0100

    bytt ut i day.js

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,19 +1,12 @@
-import moment from "moment";
-import { AFPeriode } from "../types/arbeidsforhold";
+import dayjs from 'dayjs';
+import { AFPeriode } from '../types/arbeidsforhold';
 
-export const sortDateStringDesc = (a?: string, b?: string) =>
-  a && b ? moment(b).diff(moment(a)) : !a && b ? -1 : a && !b ? 1 : 0;
+export const sortDateStringDesc = (a?: string, b?: string) => (a && b ? dayjs(b).diff(dayjs(a)) : !a && b ? -1 : a && !b ? 1 : 0);
 
-export const sortPeriodeFraDesc = (a?: AFPeriode, b?: AFPeriode) =>
-  a && b ? sortDateStringDesc(a.periodeFra, b.periodeFra) : 0;
+export const sortPeriodeFraDesc = (a?: AFPeriode, b?: AFPeriode) => (a && b ? sortDateStringDesc(a.periodeFra, b.periodeFra) : 0);
 
-export const sortPeriodeTilDesc = (a?: AFPeriode, b?: AFPeriode) =>
-  a && b ? sortDateStringDesc(a.periodeTil, b.periodeTil) : 0;
+export const sortPeriodeTilDesc = (a?: AFPeriode, b?: AFPeriode) => (a && b ? sortDateStringDesc(a.periodeTil, b.periodeTil) : 0);
 
-export const formatDate = (
-  date: string,
-  format = "DD.MM.YYYY",
-  locale = "nb"
-) => {
-  return moment(date).locale(locale).format(format);
+export const formatDate = (date: string, format = 'DD.MM.YYYY', locale = 'nb') => {
+    return dayjs(date).locale(locale).format(format);
 };

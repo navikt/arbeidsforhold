875f7dc360abcc0c88496b4961d6d36374ad4bdf
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-05-02 12:54:40 +0200

    intern.dev -> ansatt.dev

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,24 +1,24 @@
-import { Miljo } from "../types/miljo";
+import { Miljo } from '../types/miljo';
 
 export class Environment {
-  static apiUrl: string;
-  static loginUrl: string;
+    static apiUrl: string;
+    static loginUrl: string;
 
-  static settEnv = (miljo: Miljo) => {
-    switch (miljo) {
-      case "LOCAL":
-        Environment.apiUrl = `http://localhost:8096/person/arbeidsforhold-api`;
-        Environment.loginUrl = `http://localhost:5000`;
-        break;
-      case "DEV":
-        Environment.apiUrl = `https://www.intern.dev.nav.no/person/arbeidsforhold-api`;
-        Environment.loginUrl = `https://login.ekstern.dev.nav.no/oauth2/login`;
-        break;
-      case "PROD":
-        Environment.apiUrl = `https://www.nav.no/person/arbeidsforhold-api`;
-        Environment.loginUrl = `https://login.nav.no/oauth2/login`;
-        break;
-      default:
-    }
-  };
+    static settEnv = (miljo: Miljo) => {
+        switch (miljo) {
+            case 'LOCAL':
+                Environment.apiUrl = `http://localhost:8096/person/arbeidsforhold-api`;
+                Environment.loginUrl = `http://localhost:5000`;
+                break;
+            case 'DEV':
+                Environment.apiUrl = `https://www.ansatt.dev.nav.no/person/arbeidsforhold-api`;
+                Environment.loginUrl = `https://login.ekstern.dev.nav.no/oauth2/login`;
+                break;
+            case 'PROD':
+                Environment.apiUrl = `https://www.nav.no/person/arbeidsforhold-api`;
+                Environment.loginUrl = `https://login.nav.no/oauth2/login`;
+                break;
+            default:
+        }
+    };
 }

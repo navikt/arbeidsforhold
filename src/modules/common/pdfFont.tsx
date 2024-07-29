a7b80b7df710e2fa32bbb6f84fa6bda055285133
Martin Berglund <martin.berglund@nav.no>
2024-07-29 15:31:30 +0200

    Oppdaterte navn på fontfiler

--- src/modules/common/pdfFont.tsx
+++ src/modules/common/pdfFont.tsx
@@ -1,6 +1,6 @@
-import Regular from '@/assets/fonts/ODelI1aHBYDBqgeIAH2zlNRl0pGnog23EMYRrBmUzJQ.ttf';
-import Italic from '@/assets/fonts/M2Jd71oPJhLKp0zdtTvoMwRX4TIfMQQEXLu74GftruE.ttf';
-import Bold from '@/assets/fonts/toadOcfmlt9b38dHJxOBGPgXsetDviZcdR5OzC1KPcw.ttf';
+import Regular from '@/assets/fonts/Regular.ttf';
+import Italic from '@/assets/fonts/Italic.ttf';
+import Bold from '@/assets/fonts/Bold.ttf';
 
 export const pdfFont = {
     family: 'SourceSansPro',
